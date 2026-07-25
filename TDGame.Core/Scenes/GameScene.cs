b73344db76f3eda786f4c00721addc37c9b6ce48
Sondirn <sondirn@gmail.com>
2026-07-24 19:42:10 -0500

    planets

--- TDGame.Core/Scenes/GameScene.cs
+++ TDGame.Core/Scenes/GameScene.cs
@@ -11,8 +11,6 @@ public class GameScene : Scene<GameScene>
     {
         Window.SetSize(1280, 720);
         
-        RenderingOptions.SamplerState = SamplerState.PointClamp;
-        
         AmbientLight.Intensity = 1.0f;
         AmbientLight.Color = Color.White;
 
@@ -21,13 +19,11 @@ public class GameScene : Scene<GameScene>
             createAt: new Vector3(640f, 360f, 0f)
         );
         
-        MainCamera.PixelsPerUnit = 1;
+        MainCamera.PixelsPerUnit = 2;
         MainCamera.SetTargetVerticalResolution(720);
         MainCamera.ForcePosition(new Vector3(640f , 360f, 0f));
 
-        var rect = ent.AttachComponent<RectDrawer>();
-        rect.Width = 128;
-        rect.Height = 128;
-        rect.Color = Color.Magenta;
+        var rect = ent.AttachComponent<SpriteDrawer>();
+        rect.Sprite = Sprite.Create("Textures/Planets/Terran");
     }
 }
